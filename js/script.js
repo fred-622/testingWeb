0fb3f0bd127cc539fa661c8de3972a702a198a16
fred-622 <138508260+fred-622@users.noreply.github.com>
2023-07-13 20:39:15 -0400

    Updated js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ function changeText()
 {
     document.getElementById('p4').innerHTML = "Javascript text";
 }
+// Nav bar lighting the actual name of the page we are on.
+$(function()
+{
+    var pages = ['index','about','contact'];
+    var pathname = window.location.pathname;
+
+    $('.nav-link').each(function(i)
+    {
+        if(pathname.includes(pages[i]))
+        {
+            $(this).addClass('active');
+            $(this).attr('aria-current', 'page');
+        }else if(this.className.includes('active'))
+        {
+            $(this).removeClass('active');
+        }
+    });
+
+});
